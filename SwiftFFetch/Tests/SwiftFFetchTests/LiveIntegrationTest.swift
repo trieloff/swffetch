4efcf0ab87f0116397e65a477c76e2121865a514
Zed AI <noreply@zed.dev>
2025-07-11 12:09:07 +0200

    Remove Xcode-generated headers, keep copyright and license only
    Signed-off-by: Lars Trieloff <lars@trieloff.net>

--- SwiftFFetch/Tests/SwiftFFetchTests/LiveIntegrationTest.swift
+++ SwiftFFetch/Tests/SwiftFFetchTests/LiveIntegrationTest.swift
@@ -1,9 +1,5 @@
 //
-//  LiveIntegrationTest.swift
-//  SwiftFFetchTests
-//
-//  Created by SwiftFFetch on 2024-01-01.
-//  Copyright © 2024 Adobe. All rights reserved.
+//  Copyright © 2025 Adobe. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
